c275212473d834512521f7de8d0813ee773ad944
Lise Gauthier <lise@MacBook-Air-de-Lise.local>
2022-03-25 15:06:20 +0100

    initialisation front

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "@/views/Home.vue";
-import Countries from "@/views/Countries.vue";
-import CountryEdit from "@/views/CountryEdit.vue";
-import CityEdit from "@/views/CityEdit.vue";
+import Home from "@/views/admin/Home.vue";
 
 const routes = [
   {
@@ -10,30 +7,7 @@ const routes = [
     name: "Home",
     component: Home,
   },
-  {
-    path: "/countries",
-    name: "Countries",
-    component: Countries,
-  },
-  {
-    path: "/cityEdit",
-    name: "CityEdit",
-    component: CityEdit,
-  },
-  {
-    path: "/countryEdit",
-    name: "CountryEdit",
-    component: CountryEdit,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+
 ];
 
 const router = createRouter({
