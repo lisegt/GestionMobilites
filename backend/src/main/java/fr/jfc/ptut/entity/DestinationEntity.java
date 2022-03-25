fd34b8c698ecfb21115c43aaf1de8bb79f22a9f0
hugomatton <hugomattonisis@gmail.com>
2022-03-25 15:57:54 +0100

    Localistation + Destination entity

---
+++ backend/src/main/java/fr/jfc/ptut/entity/DestinationEntity.java
@@ -0,0 +1,40 @@
+package fr.jfc.ptut.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @RequiredArgsConstructor @ToString
+public class DestinationEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private Periode periode;
+
+    private int nbPlaceSemestre;
+
+    private int nbPlaceAnnee;
+
+    private LocalDate dateFinDeContratIsis;
+
+    private String nomEtablissementAccueil;
+
+    private String image;
+
+    @ManyToOne
+    private LocalisationEntity localisation;
+
+}
---
+++ backend/src/main/java/fr/jfc/ptut/entity/Periode.java
@@ -0,0 +1,8 @@
+package fr.jfc.ptut.entity;
+
+public enum Periode {
+    STAGE,
+    ETUDE_ERASMUS,
+    ETUDE_HORS_ERASMUS,
+    HUMANITAIRE;
+}
