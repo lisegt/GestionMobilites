3b062c31ac7ab6798a16e236314d13be5f18b228
Rayane Jay Bencharef <rayanejaybencharef@gmail.com>
2022-03-25 16:34:46 +0100

    ajout image accueil admin

--- frontend/src/components/CarteAccueil.vue
+++ frontend/src/components/CarteAccueil.vue
@@ -1,11 +1,14 @@
 <template>
     
-<div class="div">
+<div class="col-2 d-flex flex-column justify-content-center align-items-around">
 
+    <img src="{{chemin}}" alt="image pour {{nom}}">
+    <div class="h3"> {{nom}}</div>
 </div>
 
 </template>
 
 <script setup>
+defineProps(["chemin","nom"])
 
 </script>
---
+++ frontend/src/views/admin/Accueil/Accueil.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="container-fluid h-100 border border-2 border-red ">
+<!-- 
+<div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-view />
+      
+</div>
+-->
+
+<Carte v-bind:nom="'Destination'" v-bind:chemin="'../views/admin/Accueil/img/img_destionations.png'"/>
+</div>
+    
+</template>
+
+<script setup>
+import Carte from "../../../components/CarteAccueil.vue"
+
+
+</script>
