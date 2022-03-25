9460732cf3f0c2ea7d5dfaa0d5278d1d4075fcff
Kilian Lamula <kilian.lamula@etud.univ-jfc.fr>
2022-03-25 16:28:12 +0100

    uml

--- backend/src/main/java/fr/jfc/ptut/entity/Type.java
+++ backend/src/main/java/fr/jfc/ptut/entity/Type.java
@@ -1,14 +1,8 @@
 package fr.jfc.ptut.entity;
 
-public enum Periode {
-<<<<<<< HEAD
-    SEMESTRE1,
-    SEMESTRE2,
-    ANNEE
-=======
+public enum Type {
     STAGE,
     ETUDE_ERASMUS,
     ETUDE_HORS_ERASMUS,
     HUMANITAIRE;
->>>>>>> 490332a4756941db32ac7846580f273a82eeb398
 }
