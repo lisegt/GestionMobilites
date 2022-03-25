05058f70dd4d649a2b69a8e73b1957b6a839947e
Kilian Lamula <kilian.lamula@etud.univ-jfc.fr>
2022-03-25 16:22:56 +0100

    uml

---
+++ backend/src/main/java/fr/jfc/ptut/entity/EtudiantEntity.java
@@ -0,0 +1,31 @@
+package fr.jfc.ptut.entity;
+
+import java.util.LinkedList;
+import java.util.List;
+import javax.persistence.*;
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @RequiredArgsConstructor @ToString
+@Entity
+public class EtudiantEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NonNull
+    private int numEtud;
+    
+    @NonNull
+    private String nom;
+
+    @NonNull
+    private String prenom;
+
+    @NonNull
+    private String promo;
+
+    @OneToMany(mappedBy="etudiant")
+    @ToString.Exclude // On ne veut pas inclure la liste des mob dans le toString
+    // Sinon récursivité infinie 
+    private List<MobiliteEntity> mobilites = new LinkedList<>();
+}
