836e317b34d84a1d48b6593463c784e04eb3411c
Lise Gauthier <lise@MacBook-Air-de-Lise.local>
2022-04-04 16:42:57 +0200

    debug

--- frontend/src/components/Carousel.vue
+++ frontend/src/components/Carousel.vue
@@ -3,13 +3,13 @@
  <div id="carouselExampleSlidesOnly" class="carousel slide h-100 " data-bs-ride="carousel">
   <div class="carousel-inner h-100 ">
     <div class="carousel-item h-100 active">
-      <img class="d-block w-100 h-100 round" src="../views/admin/Accueil/img/paysage6.jpg" alt="...">
+      <img class="d-block w-100 h-100 round" src="../views/admin/accueil/img/paysage6.jpg" alt="...">
     </div>
     <div class="carousel-item h-100">
-      <img  class="d-block w-100 h-100 round" src="../views/admin/Accueil/img/paysage5.jpg" alt="...">
+      <img  class="d-block w-100 h-100 round" src="../views/admin/accueil/img/paysage5.jpg" alt="...">
     </div>
     <div class="carousel-item h-100">
-      <img  class="d-block w-100 h-100 round" src="../views/admin/Accueil/img/paysage4.jpg" alt="...">
+      <img  class="d-block w-100 h-100 round" src="../views/admin/accueil/img/paysage4.jpg" alt="...">
     </div>
   </div>
 </div>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-
-
 // bootstrap 5
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
 createApp(App).use(store).use(router).mount("#app");
