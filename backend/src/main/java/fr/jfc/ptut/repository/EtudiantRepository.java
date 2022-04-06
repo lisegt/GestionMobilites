4cd86d2c1efeb4e9a9c749295e3a9712f96dae7f
hugomatton <hugomattonisis@gmail.com>
2022-04-06 11:19:21 +0200

    commit

--- backend/src/main/java/fr/jfc/ptut/repository/EtudiantRepository.java
+++ backend/src/main/java/fr/jfc/ptut/repository/EtudiantRepository.java
@@ -1,8 +1,10 @@
 package fr.jfc.ptut.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import fr.jfc.ptut.entity.Etudiant;
 
 public interface EtudiantRepository extends JpaRepository<Etudiant, Integer> {
-    
+    Optional<Etudiant> findByNom(String nom);
 }
