af53d00d6e328e8fe604edbb5ef36de196894f73
Lise Gauthier <lise@MacBook-Air-de-Lise.local>
2022-04-06 11:15:17 +0200

    renommage dossiers

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,9 +3,29 @@ import Accueil from "@/views/admin/accueil/Accueil.vue";
 
 const routes = [
   {
-    path: "/",
+    path: "/admin",
     name: "Accueil",
     component: Accueil ,
+  },
+
+  { path: "/admin/etudiants",
+    name: "Etudiants",
+    component: Accueil,
+  },
+
+  { path: "/admin/destinations",
+    name: "Destinations",
+    component: Accueil,
+  },
+
+  { path: "/admin/mobilites",
+    name: "Mobilites",
+    component: Accueil,
+  },
+
+  { path: "/admin/documents",
+    name: "Documents",
+    component: Accueil,
   },
 
 ];
