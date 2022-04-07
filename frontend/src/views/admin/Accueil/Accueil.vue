34e8f2258565b7459b4fda840b65687c0dd78dde
Rayane Jay Bencharef <rayanejaybencharef@gmail.com>
2022-04-07 10:05:24 +0200

    reactif

--- frontend/src/views/admin/Accueil/Accueil.vue
+++ frontend/src/views/admin/Accueil/Accueil.vue
@@ -1,11 +1,11 @@
 <template>
 
-      <div id="accueil" class=" mb-0 d-flex flex-column justify-content-around  ">
+      <div id="accueil" class="overflow-scroll mb-0 d-flex flex-column justify-content-around  ">
             <div class="col-12 col-md-6 m-auto hauteur">
                   <Carousel/>
             </div>
             <div class="col-12 col-md-6  m-auto">
-                  <div class="row largeur  pb-5 pt-0 pb-md-5  justify-content-around g-3 gy-xl-0 gx-md-1">    
+                  <div class=" row largeur border  pb-5 pt-0 pb-md-5  justify-content-around g-3 gy-xl-0 gx-md-1">    
 
                         <Carte v-bind:nom="'ETUDIANTS'" v-bind:chemin="etudiants"/>
                         <Carte v-bind:nom="'DESTINATIONS'" v-bind:chemin="destinations"/>
@@ -18,9 +18,10 @@
 </template>
 
 <style>
+
 #accueil{
       height: 85%;
-      overflow-y:auto;
+      overflow: scroll;
      
 
 }
@@ -29,11 +30,16 @@
  height: 50vh;
  
 }
+
 @media all and (orientation: portrait) {
       .hauteur{
 
             height: 28vh;
       }
+
+      
+
+     
 
 }
 
