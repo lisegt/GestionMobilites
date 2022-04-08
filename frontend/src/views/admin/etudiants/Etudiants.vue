65449d0d2689f70c03365b881622cb92c6d70b78
Kilian Lamula <kilian.lamula@etud.univ-jfc.fr>
2022-04-08 09:48:40 +0200

    Recherche étudiant

--- frontend/src/views/admin/etudiants/Etudiants.vue
+++ frontend/src/views/admin/etudiants/Etudiants.vue
@@ -1,24 +1,38 @@
 <template>
   <h1>Gestion des étudiants</h1>
-  <button @click="test">test</button>
 
   <FormAddEtud/>
+
+  <SearchEtud v-bind:etudiants="listeEtudiants"/>
 
 </template>
 
 <script setup>
   import FormAddEtud from './formAddEtud/FormAddEtud.vue'
   import FormModifEtud from './formModifEtud/FormModifEtud.vue'
+  import SearchEtud from './searchEtud/SearchEtud.vue'
+
+  import { onMounted, onUpdated } from "vue";
 
   import { reactive } from 'vue';
+  //Liste d'étudiants
   const listeEtudiants = reactive([]);
-  
 
-  function test(){
-    fetch("/api/etudiant/mobilite/1", {method: 'GET'})
-    .then((response)=>response.json())
-    .then((dataJson)=> console.log(dataJson))
-  }
+  onMounted(() => {
+    getToDos()
+});
+
+function getToDos(){
+    const fetchOptions = {method:"GET"};
+    fetch("http://localhost:8989/api/etudiants/",fetchOptions)
+    .then((response) => {return response.json()})
+    .then((dataJSON) => {
+      listeEtudiants.splice(0,listeEtudiants.length)
+      dataJSON._embedded.etudiants.forEach((e)=>listeEtudiants.push(e))
+      console.log(listeEtudiants)
+    })
+    .catch((error) => console.log(error));
+}
   
 </script>
 
--- frontend/src/views/admin/etudiants/searchEtud/SearchEtud.vue
+++ frontend/src/views/admin/etudiants/searchEtud/SearchEtud.vue
@@ -1,9 +1,37 @@
 <template>
-  
+<div id="form">
+    <form @submit.prevent="search()">
+        <input id="search" type="text" v-model="nom" placeholder="Rechercher un étudiant ..." required/>
+        <br>
+        <input id="Bouton" type="submit" value="Search" />
+    </form>
+</div>
 </template>
 
-<script>
+<script setup>
+/**
+ * @param
+ * @return
+ * Fonction qui recherche un étudiant par méthode GET avec son nom
+ */
 
+import { reactive } from 'vue';
+//Liste reactive pour la recherche :
+const etudiantSearch = reactive([]);
+
+let etudiants= defineProps(["etudiants"]);
+console.log(etudiants.etudiants)
+function search(){
+    etudiantSearch.splice(0,etudiantSearch.length)
+    let nom = document.getElementById("search").value
+    
+    for(let e of etudiants.etudiants){
+        if(e.nom.includes(nom)){
+            etudiantSearch.push(e)
+        }
+    }
+    console.log(etudiantSearch)
+}
 </script>
 
 <style>
