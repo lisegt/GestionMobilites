f5365f1e816c9eacd727832838e6aba2cb722372
Kilian Lamula <kilian.lamula@etud.univ-jfc.fr>
2022-04-11 22:44:26 +0200

    form ajout docs admin fonctionnel

--- frontend/src/views/admin/docsAdmin/DocsAdmin.vue
+++ frontend/src/views/admin/docsAdmin/DocsAdmin.vue
@@ -1,22 +1,26 @@
 <template>
+<div>
   <h1>Gestion des documents administratifs</h1>
-
   <FormModifDocsAdmin @update_ok="getDoc"/>
+
+  <button type="button" class="btnOrange " data-bs-toggle="modal" data-bs-target="#ajout">
+    Ajouter un document
+  </button>
+  <FormAddDocsAdmin @post="addDoc"/>
+
   <TableDocsAdmin @update="setDoc" @delete="deleteDocument" v-bind:documents="listeDocuments"/>
 
+</div>
 </template>
 
 <script setup>
 import TableDocsAdmin from "./tableDocsAdmin/TableDocsAdmin.vue";
 import FormModifDocsAdmin from './formModifDocsAdmin/FormModifDocsAdmin.vue'
+import FormAddDocsAdmin from './formAddDocsAdmin/FormAddDocsAdmin.vue'
 import { onMounted, onUpdated } from "vue";
 import { reactive } from 'vue';
 //Liste de documents
 const listeDocuments = reactive([]);
-
-onMounted(() => {
-  getDoc()
-});
 
 /**
  * 
@@ -36,6 +40,12 @@ function getDoc(){
   .catch((error) => console.log(error));
 }
 
+/**
+ * 
+ * @param id
+ * @return
+ * Fonction qui permet de supprimer un document à partir de son id
+ */
 function deleteDocument(id){
   let url = `http://localhost:8989/api/documents/`
   
@@ -51,13 +61,27 @@ function deleteDocument(id){
  * 
  * @param doc
  * @return
- * Fonction qui récupère les données pas encore modifiées
+ * Fonction qui modifie les données
  */
 function setDoc(doc){
   document.getElementById("intitule").value = doc.intitule
   document.getElementById("description").value = doc.description
   document.getElementById('idDocToEdit').value= doc.id
 }
+
+function addDoc(intitule, description){
+  const url = `/api/documents` 
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  const fetchOptions = {method:"POST", headers: myHeaders, body: JSON.stringify({intitule:intitule, description:description})};
+  fetch(url,fetchOptions)
+  .then((response) => getDoc())
+  .catch((error) => console.log(error));
+}
+
+onMounted(() => {
+  getDoc()
+});
 </script>
 
 <style>
--- frontend/src/views/admin/docsAdmin/formAddDocsAdmin/FormAddDocsAdmin.vue
+++ frontend/src/views/admin/docsAdmin/formAddDocsAdmin/FormAddDocsAdmin.vue
@@ -1,11 +1,68 @@
 <template>
-  
+<div class="modal fade" id="ajout" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title font-weight-bold" id="Label">Ajouter un document</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div id="form">
+            <form>
+                <div class="form-group">
+                    <label for="nom" class="font-weight-bold">Nom du document :</label>
+                    <input v-model="intitule" id="intitule" class="form-control" name="intitule" type="text" placeholder="Entrez le nom du document ..." required/>
+                </div>
+                <div class="form-group">
+                    <label for="prenom" class="font-weight-bold">Description :</label>
+                    <input v-model="description" id="description" class="form-control" name="description" type="text" placeholder="Entrez une description du document ..." required/>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btnOrange" data-bs-dismiss="modal">Close</button>
+                    <input @click="$emit('post', intitule, description)" id="bouton" type="submit" class="btnOrange" value="Ajouter"  data-bs-dismiss="modal"/>
+                 </div>
+            </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
-<script>
+<script setup>
+import {ref} from 'vue'
 
+let intitule = ref('')
+let description = ref('')
 </script>
 
-<style>
-
+<style scoped>
+h5{
+    font-size: 30px;
+    line-height: 36px;
+}
+label{
+    font-size: 24px;
+    line-height: 29px;
+}
+h5,label{
+    text-transform: uppercase;
+    color: #022E51;
+    font-family: 'Bebas Neue';
+    font-weight: 400;
+    font-style: normal;
+    text-align: center;
+}
+.form-control{
+    background: rgba(163, 180, 200, 0.35);
+    border-radius: 10px;
+    font-family: 'Montserrat';
+    font-weight: 400;
+    font-size: 20px;
+    color: #022E51;
+}
+#ajout{
+    border-radius: 10px;
+}
 </style>
