ec520197b4ae0ec7e9ab01995e929c052ad6734c
hugomatton <hugomattonisis@gmail.com>
2022-04-12 14:53:47 +0200

    modif data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -10,7 +10,7 @@ INSERT INTO ETUDIANT (nom, num_etud, prenom, promo) VALUES
 
 INSERT INTO DESTINATION (date_fin_de_contrat_isis,image,nb_place_annee,nb_place_semestre,nom_etablissement_accueil,type_mobilite,ville,pays) VALUES
     ('2025-02-02', 'image', 4, 2, 'Artic University', 1,'Tromso','Norvege'),
-    ('2025-02-02', 'image', 4, 2, 'European University Of Cyprus', 1,'Nicosia','Chypre');
+    ('2022-09-02', 'image', 4, 2, 'European University Of Cyprus', 1,'Nicosia','Chypre');
 
 INSERT INTO MOBILITE (date_depart, duree_en_mois, periode, destination_id, etudiant_id) VALUES
     ('2023-04-10', 5, 1, SELECT ID FROM DESTINATION WHERE nom_etablissement_accueil = 'European University Of Cyprus', SELECT ID FROM ETUDIANT WHERE nom = 'BEN' );
