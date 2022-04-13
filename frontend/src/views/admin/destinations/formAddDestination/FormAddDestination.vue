67667b72a501b383282685ec25b42ccfbac268bf
Kilian Lamula <kilian.lamula@etud.univ-jfc.fr>
2022-04-13 11:56:32 +0200

    form modifier destination design

--- frontend/src/views/admin/destinations/formAddDestination/FormAddDestination.vue
+++ frontend/src/views/admin/destinations/formAddDestination/FormAddDestination.vue
@@ -3,7 +3,7 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-            <   h5 class="modal-title font-weight-bold" id="Label">Ajouter une destination</h5>
+            <h5 class="modal-title font-weight-bold" id="Label">Ajouter une destination</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div id="form">
