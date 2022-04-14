ebf82efa707423b75c136799c342098fc63fc23c
Kilian Lamula <kilian.lamula@etud.univ-jfc.fr>
2022-04-14 23:05:09 +0200

    accueil site public

---
+++ frontend/src/components/CarteAccueilEtud.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="col-6 col-xl-3 ">
+    <a type="button" v-bind:href="lien"  class=" text-white py-3 paddingX px-md-0 mx-2 bg-color d-flex flex-column justify-content-center align-items-center border-0 ">
+        <img v-bind:src="chemin" width="140" height="100" alt="image ">
+        <div class="h4 mt-3"> {{nom}}</div>
+    </a>
+</div>
+</template>
+
+<script setup>
+defineProps(["chemin","nom","lien"])
+</script>
+
+<style>
+#carte{
+    background-color: #B74803;
+    border-radius: 50px;
+    color: #F3F3F3;
+    font-family: "Bebas Neue";
+    font-size: 30px;
+}
+.h4{
+    text-align: center;
+    padding-left: 20px;
+    padding-right: 20px;
+}
+</style>
--- frontend/src/views/etudiant/accueil/Accueil.vue
+++ frontend/src/views/etudiant/accueil/Accueil.vue
@@ -1,11 +1,43 @@
 <template>
-  
+<div id="accueil" class="overflow-auto mb-0 d-flex flex-column justify-content-around  ">
+    <div class="col-12 col-md-6 m-auto hauteur">
+        <Carousel/>
+    </div>
+    <div class="col-10 m-auto justify-content-center">
+        <div class="row justify-content-center gx-5 py-5">    
+            <Carte v-bind:nom="'DESTINATIONS'" v-bind:lien="'/etudiant/destinations'" v-bind:chemin="destinations"/>
+            <Carte v-bind:nom="nomDoc" v-bind:lien="'/etudiant/documents'" v-bind:chemin="doc"/>
+        </div>
+        
+    </div>
+</div>
 </template>
 
-<script>
+<script setup>
+import Carte from "../../../components/CarteAccueilEtud.vue"
+import Carousel from "../../../components/Carousel.vue"
 
+import destinations from "../../admin/accueil/img/img_destinations.png" 
+import doc from "../../admin/accueil/img/img_docsadmin.png"
+
+let nomDoc= "DOCUMENTS ADMIN."
+if(window.innerWidth < window.innerHeight  ){
+    nomDoc="DOCUMENTS"
+}
 </script>
 
 <style>
+#accueil{
+    height: 85%;
+    overflow: auto;
+}
+.hauteur{
+    height: 50vh;
+}
 
+@media all and (orientation: portrait) {
+    .hauteur{
+        height: 28vh;
+    }  
+}
 </style>
