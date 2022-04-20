4123e30cd5bdda50ad76615f4e96498be98405f2
Kilian Lamula <kilian.lamula@etud.univ-jfc.fr>
2022-04-20 18:10:29 +0200

    côté etudiant destination popup

--- frontend/src/views/etudiant/destinations/popupInfo/PopupInfo.vue
+++ frontend/src/views/etudiant/destinations/popupInfo/PopupInfo.vue
@@ -1,29 +1,34 @@
 <template>
-<div class="modal fade h-75" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade h-100" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg h-100">
     <div class="modal-content">
-      <div class="modal-header border-0">
-        <div class="col m-auto justify-content-center">
+      <div class="modal-header border-0 row py-0">
+        <div class="col-1"></div>
+        <div class="col-10 text-center mt-3">
             <h5 class="modal-title font-weight-bold justify-content-center" id="Label">{{destination.nomEtablissementAccueil}} <h6>{{destination.ville}}, {{destination.pays}}</h6></h5>
         </div>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <button type="button" class="btn-close col-1 me-4" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body h-25">
-          <img v-bind:id="index" v-bind:src="img" class="img" width="170" height="190" alt="image ">
-          <div class="row">
-            <div class="titre col">Type Mobilité :</div>
-            <div class="valeur col">mobilité {{destination.typeMobilite}}</div>
+      <div class="modal-body h-25 contenu">
+          <div class="col text-center">
+            <img v-bind:id="index" v-bind:src="img" class="img" width="170" height="190" alt="image ">
           </div>
-          <div class="row">
-            <div class="col titre">Nombre de places disponibles : </div>
-            <div class="col valeur">{{destination.nbPlaceAnnee}} place(s) par an / {{destination.nbPlaceSemestre}} place(s) par semestre</div>
-          </div>
-          <div class="row">
-            <div class="col titre">Etudiants ayant effectués cette mobilité :</div>
-            <ul>
-                <li>
-                </li>
-            </ul>
+          <div class="col justify-content-center my-4">
+            <div class="row">
+              <div class="titre col">Type Mobilité :<span class="col valeur">Mobilité {{destination.typeMobilite}}</span></div>
+            </div>
+            <div class="row">
+              <div class="col titre">Nombre de places disponibles :<span class="col valeur">{{destination.nbPlaceAnnee}} place(s) par an / {{destination.nbPlaceSemestre}} place(s) par semestre</span></div>
+            </div>
+            <div class="row">
+              <div class="col titre">Retour(s) d'expérience :</div>
+              <span>
+                <ul>
+                    <li>
+                    </li>
+                </ul>
+              </span>
+            </div>
           </div>
       </div>
     </div>
@@ -47,20 +52,22 @@ let type = ref({})
  * Sinon image par défaut selon le type de mobilité
  */
 function Base64ToImage(base64img) {
-    if(props.img==""){
-        if(props.type=="STAGE"){
-            document.getElementById(`${props.index}`).src=stage
-        }
-        if(props.type=="ETUDE"){
-            document.getElementById(`${props.index}`).src=etude
-        }
-        if(props.type=="HUMANITAIRE"){
-            document.getElementById(`${props.index}`).src=humanitaire
-        }
+  if(props.img==""){
+    
+    if(props.type=="STAGE"){
+      console.log(props.type)
+      document.getElementById(`${props.index}`).src=stage
     }
-    else{
-        document.getElementById(`${props.index}`).src=props.img
+    if(props.type=="ETUDE"){
+      document.getElementById(`${props.index}`).src=etude
     }
+    if(props.type=="HUMANITAIRE"){
+      document.getElementById(`${props.index}`).src=humanitaire
+    }
+  }
+  else{
+      document.getElementById(`${props.index}`).src=props.img
+  }
 }
 
 /**
@@ -79,8 +86,10 @@ h6{
     font-size: 24px;
 }
 .valeur{
-    text-transform: lowercase;
     font-family: "Montserrat";
-    font-size: 24px;
+    font-size: 20px;
+}
+img{
+  height: auto;
 }
 </style>
