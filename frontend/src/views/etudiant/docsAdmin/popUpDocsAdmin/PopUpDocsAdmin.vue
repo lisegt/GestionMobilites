ee5c8bbc0128f2846e88d80c13aa8a75ea57ad80
Rayane Jay Bencharef <rayanejaybencharef@gmail.com>
2022-04-22 00:42:32 +0200

    doc admin pop up

--- frontend/src/views/etudiant/docsAdmin/popUpDocsAdmin/PopUpDocsAdmin.vue
+++ frontend/src/views/etudiant/docsAdmin/popUpDocsAdmin/PopUpDocsAdmin.vue
@@ -3,18 +3,18 @@
   <div class="modal-dialog modal-lg">
     <div class="modal-content h-50">
       <div class="modal-header border-0 row">
-          <div class="col-1"></div>
+          <div class="col-xl-1"></div>
           <div class="modal-title text-center col-10 h3" >{{document.intitule}}</div>
           <button type="button" class="btn-close col-1 me-3" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
+      </div> 
       <div class="modal-body h-25 justify-content-center">
           <div class="row h-50 overflow-y-auto">
-              <div class="h4 text-left col-2">Description :</div>
-              <div class="col-10 text-left h-100 overflow-auto font-weight-bold" style="word-wrap:break-word;">{{document.description}}</div>
+              <div class="h4 text-left col-4 col-xl-2">Description :</div>
+              <div class="col-8 col-xl-10 text-left h-100 overflow-auto font-weight-bold" style="word-wrap:break-word;">{{document.description}}</div>
           </div>
           <div class="row mt-3 h-25 align-items-center justify-content-start">
-              <div class="col-4 h4 text-left">Lien de telechargement : </div>
-              <div  class="col-8 text-left h-100   align-items-center ">
+              <div class="col-12 col-xl-4 h4 text-left">Lien de telechargement : </div>
+              <div  class="col-12 col-xl-8 text-left h-100   align-items-center ">
                   <a @click="download" id="btnDownLoad" type="button" class="text-center btn bordureBtn font-weight-bold" >
                   Cliquez pour télécharger
                   </a>
